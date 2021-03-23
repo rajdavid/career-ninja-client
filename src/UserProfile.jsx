29d697d48770d726b7592a5c744ddaa9bf14c43d
david raj <david.raj@techchefz.com>
2021-03-23 23:36:11 +0530

    user data maping

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -1,111 +1,66 @@
-import logo from './logo.svg';
-import './dashboard.scss';
 
+import "./dashboard.scss";
+import React from 'react'
+import endpoint from './utility/endpoint'
+import axios from 'axios'
 function UserProfile() {
+  const [tableData, setTableData] = React.useState();
+  React.useEffect(async () => {
+    const alltableDatta = await axios.get(endpoint.getAllDataApi);
+    console.log('alltable data --', alltableDatta.data.data);
+    setTableData(alltableDatta.data.data);
+  },[])
+
   return (
     <div className="dashboard">
       <div className="left">
         <div className="divimage">
-        <img src="/career.png" />
+          <img src="/career.png" />
         </div>
-     
-         <div className="list">
-           <ul>
-             <li>Dashboard</li>
-             <li>Users</li>
-             <li>Admin Users</li>
-             <li>Whatsapp</li>
-           </ul>
-         </div>
-         <span className="actionbtn">LOGOUT</span>
+
+        <div className="list">
+          <ul>
+            <li>Dashboard</li>
+            <li>Users</li>
+            <li>Admin Users</li>
+            <li>Whatsapp</li>
+          </ul>
+        </div>
+        <span className="actionbtn">LOGOUT</span>
       </div>
       <div className="right">
-      <div className="top">
-        <span className="topbtn">
-          Add User
-        </span>
+        <div className="top">
+          <span className="topbtn">Add User</span>
+        </div>
+        <div className="middle">
+          <p className="head">Home</p>
+          <p className="content">Admin-Users</p>
+        </div>
+        <div className="bottom">
+          <table>
+            <thead>
+              <tr>
+                <th>Index</th>
+                <th>Name</th>
+                <th>Mobile</th>
+                <th>Email</th>
+                <th>Usertype</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((user, index) => (
+                <tr>
+                <td>{index+1}</td>
+                <td>{user.name}</td>
+                <td>{user.phoneNumber}</td>
+                <td>{user.email}</td>
+                <td>{user.userType}</td>
+              </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="middle">
-        <p className="head">Home</p>
-        <p className="content">Admin-Users</p>
-      </div>
-      <div className="bottom">
-     
-                <table>
-                    <thead>
-                        <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Mobile</th>
-                        <th>Email</th>
-                        <th>Username</th>
-                        <th>Usertype</th>
-                      
-                        <th>Mentees</th>
-                      
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                    <td>1</td>
-                        <td>North America</td>
-                        <td>Supply chain</td>
-                        <td>Hari Govind2</td>
-                        <td>Anuradha</td>
-                        <td>Accelerate</td>
-                      
-                        <td>ParulK</td>
-                  
-                    </tr>
-                    <tr>
-                    <td>1</td>
-                        <td>North America</td>
-                        <td>Supply chain</td>
-                        <td>Hari Govind2</td>
-                        <td>Anuradha</td>
-                        <td>Accelerate</td>
-                    
-                        <td>Paarul K</td>
-                       
-                    </tr>
-                    <tr>
-                    <td>1</td>
-                        <td>North America</td>
-                        <td>Supply chain</td>
-                        <td>Hari Govind2</td>
-                        <td>Anuradha</td>
-                        <td>Accelerate</td>
-                       
-                        <td>Paarul K</td>
-                      
-                    </tr>
-                    <tr>
-                    <td>1</td>
-                        <td>North America</td>
-                        <td>Supply chain</td>
-                        <td>Hari Govind2</td>
-                        <td>Anuradha</td>
-                        <td>Accelerate</td>
-                    
-                        <td>Paarul K</td>
-                       
-                    </tr>
-                    <tr>
-                    <td>1</td>
-                        <td>North America</td>
-                        <td>Supply chain</td>
-                        <td>Hari Govind2</td>
-                        <td>Anuradha</td>
-                        <td>Accelerate</td>
-                      
-                        <td>Paarul K</td>
-                      
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-            </div>
-      
     </div>
   );
 }
---
+++ src/utility/endpoint.js
@@ -0,0 +1,3 @@
+export default {
+    getAllDataApi:"http://localhost:4000/getAllUsers"
+}
